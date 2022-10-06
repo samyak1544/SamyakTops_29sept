fc55372f15fc170b0eff77728b7b1e5d23d76f33
samyak1544 <samyak1544@gmail.com>
2022-10-06 19:19:24 +0530

    Module-1

---
+++ c/Assigment/Module1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main()
+{
+	printf(" Name : samyak shah\n ");
+	printf("Birth Date : 15/04/2022\n");
+	printf(" Age : 18\n");
+	printf(" Address : 195/B,Sectore-5 Nirnaynagar,\n\t   Ahemedabad");
+}
